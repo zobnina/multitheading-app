a491037dcc428cc8ac5429eb53c9eabba65d44aa
Aleksandra Zobnina <azobnina@neoflex.ru>
2020-11-26 14:32:25 +0300

    add logger props

--- src/main/java/org/example/multithreadapp/Starter.java
+++ src/main/java/org/example/multithreadapp/Starter.java
@@ -1,7 +1,14 @@
 package org.example.multithreadapp;
 
-public class Starter {
-    public static void main(String[] args) {
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Starter {
+    static Logger logger = LogManager.getLogger(Starter.class);
+
+    public static void main(String[] args) {
+logger.error("hi");
+logger.info("hi");
     }
 }
